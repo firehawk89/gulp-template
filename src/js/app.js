5d62217937690587e71c203cdcd550da2f8682d7
Bochkovskyi <unundertale1@ukr.net>
2023-08-30 18:00:36 +0300

    update files to use es6 modules

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import btnActions from "./components/btn";
+import { mainSlider } from "./components/slider.js";
+
+console.log("Hello from app.js!");
+document.title = "GULP";
+
+btnActions();
--- src/js/components/btn.js
+++ src/js/components/btn.js
@@ -1,5 +1,6 @@
-console.log("btn.js");
-document.querySelector(".btn").addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("btn clicked");
-});
+export default function btnActions() {
+  document.querySelector(".btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    alert("You have clicked a button!");
+  });
+}
